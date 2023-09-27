19f7bc557ea080074dcdebd10654f05c0e523585
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-26 22:20:18 -0300

    CategoryResource and Entity Category

---
+++ src/main/java/com/mattcode/dscatalog/entities/Category.java
@@ -0,0 +1,53 @@
+package com.mattcode.dscatalog.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    public Category(){
+
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Category setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/com/mattcode/dscatalog/resource/CategoryResource.java
@@ -0,0 +1,24 @@
+package com.mattcode.dscatalog.resource;
+
+import com.mattcode.dscatalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L,"Games"));
+        list.add(new Category(2L,"Books"));
+        return ResponseEntity.ok().body(list);
+    }
+
+}
