e5e2d68218e96260abe64de44baddc78855e8453
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-10-01 14:04:24 -0300

    findById categories

--- src/main/java/com/mattcode/dscatalog/services/CategoryService.java
+++ src/main/java/com/mattcode/dscatalog/services/CategoryService.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -18,10 +18,17 @@ public class CategoryService {
 
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
-
         List<Category> list = repository.findAll();
-
        return list.stream().map(CategoryDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+
+        Optional<Category> category = repository.findById(id);
+        Category entity = category.get();
+
+        return new CategoryDTO(entity);
 
     }
 }
